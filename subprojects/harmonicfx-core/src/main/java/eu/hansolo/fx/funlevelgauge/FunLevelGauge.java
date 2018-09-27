2515f56e3036a66ee63e57f108a5d1511b772aed
aalmiray <aalmiray@gmail.com>
2018-09-27 16:25:29 +0200

    Multiple updates

--- subprojects/harmonicfx-core/src/main/java/eu/hansolo/fx/funlevelgauge/FunLevelGauge.java
+++ subprojects/harmonicfx-core/src/main/java/eu/hansolo/fx/funlevelgauge/FunLevelGauge.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2016 Gerrit Grunwald.
+ * Copyright 2013-2018 Gerrit Grunwald.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
