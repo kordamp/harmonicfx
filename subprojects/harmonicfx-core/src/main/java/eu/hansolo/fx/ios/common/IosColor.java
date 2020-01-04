b5797b0279ad93706cf9c8c9971dcd9fde036f6e
Andres Almiray <andres@gradle.com>
2020-01-04 15:51:53 +0100

    Update license headers; update build settings

--- subprojects/harmonicfx-core/src/main/java/eu/hansolo/fx/ios/common/IosColor.java
+++ subprojects/harmonicfx-core/src/main/java/eu/hansolo/fx/ios/common/IosColor.java
@@ -1,5 +1,7 @@
 /*
- * Copyright 2013-2018 Gerrit Grunwald.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2013-2020 The original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
